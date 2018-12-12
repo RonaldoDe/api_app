546f5adf8b0d1c06ff0346598e3c27a1a15a753c
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2018-12-12 11:42:04 -0500

    inicio

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Laravel\Passport\Client;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+
+class LoginController extends Controller
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = Client::find(1);
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator=\Validator::make($request->all(),[
+    		'username' => 'required|email',
+    		'password' => 'required|min:6',
+
+        ]);
+
+        if($validator->fails())
+        {
+         
+          //return response()->json(['errors'=>$validator->errors()->all()]);
+          // return response()->json( $datos='nO ' );
+          return response()->json( $errors=$validator->errors()->all(), 401);
+        }
+        else
+        {
+            $user=DB::table('users as u')->where('u.email',request('username'))
+            ->first();
+            if($user != null){
+                if (Hash::check(request('password'), $user->password)) {
+                    DB::table('oauth_access_tokens')->where('user_id', $user->id)->delete();
+
+                    $validar_token = DB::table('oauth_access_tokens')->where('user_id', $user->id)->first();
+                    $params = [
+                        'grant_type' => 'password',
+                        'client_id' => $this->client->id,
+                        'client_secret' => $this->client->secret,
+                        'username' => request('username'),
+                        'password' => request('password'),
+                        'scope' => '*'
+                    ];
+
+                    $request->request->add($params);
+                    $proxy = Request::create('oauth/token', 'POST');
+
+                    return Route::dispatch($proxy);
+            }else{
+                $validar_token = DB::table('oauth_access_tokens')->where('user_id', $user->id)->first();
+                $params = [
+                    'grant_type' => 'password',
+                    'client_id' => $this->client->id,
+                    'client_secret' => $this->client->secret,
+                    'username' => request('username'),
+                    'password' => request('password'),
+                    'scope' => '*'
+                ];
+
+                $request->request->add($params);
+                $proxy = Request::create('oauth/token', 'POST');
+
+                return Route::dispatch($proxy);
+            }
+        }else{
+            return response()->json(['message' => 'Usuario no encontrado'], 201);
+        }
+            
+            
+
+            
+            //return response()->json( $datos=['datos'] );
+
+    
+        }
+       
+
+    }
+
+    public function refresh(Request $request)
+    {
+        $this->validate($request, [
+            'refresh_token' => 'required'
+        ]);
+        
+        $params = [
+            'grant_type' => 'refresh_token',
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'username' => request('username'),
+            'password' => request('password')
+        ];
+
+        $request->request->add($params);
+
+         $proxy = Request::create('oauth/token', 'POST');
+
+        return Route::dispatch($proxy);
+    }
+
+
+    public function logout(Request $request)
+    {
+      $accessToken = Auth::user()->token();
+
+      DB::table('oauth_access_tokens')->where('user_id', Auth::id())->delete();
+      return response()->json(['message' => 'La sesion a sido cerrada con exito'], 200);
+
+    }
+}
